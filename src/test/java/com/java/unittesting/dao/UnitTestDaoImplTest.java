671501533127c314c3a0256cbb22161521ac4574
Arun Kumar <dahiya.akd94@gmail.com>
2022-12-29 22:56:38 +0530

    Added annotations in Service Impl Test

---
+++ src/test/java/com/java/unittesting/dao/UnitTestDaoImplTest.java
@@ -0,0 +1,14 @@
+package com.java.unittesting.dao;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitTestDaoImplTest {
+
+	UnitTestDaoImpl daoImpl = new UnitTestDaoImpl();
+
+	@Test
+	public void testGetAllData() {
+
+	}
+
+}
--- src/test/java/com/java/unittesting/service/UnitTestServiceImplTest.java
+++ src/test/java/com/java/unittesting/service/UnitTestServiceImplTest.java
@@ -4,20 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.java.unittesting.dao.UnitTestDao;
 
+@ExtendWith(MockitoExtension.class)
 public class UnitTestServiceImplTest {
 
+	@InjectMocks
 	UnitTestServiceImpl serviceImpl = new UnitTestServiceImpl();
+
+	@Mock
 	UnitTestDao unitTestDao = mock(UnitTestDao.class);
 
-	@BeforeEach
-	public void before() {
-		serviceImpl.setUnitTestDao(unitTestDao);
-	}
+	/** Not needed if we use @InjectMock annotation **/
+//	@BeforeEach
+//	public void before() {
+//		serviceImpl.setUnitTestDao(unitTestDao);
+//	}
 
 	@Test
 	public void testGetSumOfRecords() {
